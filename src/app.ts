0dc11840762dd7453de6672e4feba8db32b1af10
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-06 09:40:07 -0700

    rest params

--- src/app.ts
+++ src/app.ts
@@ -47,3 +47,13 @@ function createOrder(pizza2: any , toppings2: any) {
 }
 
 console.log(createOrder(pizza2,toppings2));
+
+
+function sumAll(message: string, ...arr: number[]) {
+  console.log(message);
+  return arr.reduce((prev, next) => prev + next);
+}
+
+const sum = sumAll('Hello!', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+console.log(sum); 
