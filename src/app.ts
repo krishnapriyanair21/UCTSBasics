903cfad9a7174443ffbb57aafd2c2d05a8edd74c
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-06 09:23:37 -0700

    arrow function

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-console.log('Hey TypeScript!');
+const pizzas = [
+  { name: 'Pepperoni', toppings: ['pepperoni'] }
+];
+
+// const mappedPizzas = pizzas.map(pizza => { /// map function and return explicitly
+//   return pizza.name.toUpperCase(); 
+// });
+
+const mappedPizzas = pizzas.map(pizza => { pizza.name.toUpperCase() }); ///implicit return
+console.log(mappedPizzas);
+
+
+const pizza = {
+  name: 'Blazing Inferno',
+  getName: function () {
+    setTimeout( () => { /// arrow functions ignore scope
+      console.log(this.name); 
+    }, 100);
+  },
+};
+
+console.log(pizza.getName());
