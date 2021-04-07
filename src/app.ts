7fe6c54832d1c24c29efbeb436f169fad2b9620e
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-07 09:43:01 -0700

    any type

--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,8 @@
-/// number types, aruguments and functions
-      // Number, Boolean, String (Java types)
+/* any */
+let coupon; // implied any
 
-const pizzaCost = 10;  // infered type number
-let tempVar: number = 12; // explict type
+coupon = 25; // number
+coupon = '25'; // string
+coupon = true;  // bool
 
- // pizzaCost = '25'; /// type string is not assignable to number
-
-const pizzaToppings: number = 2;
-
-function calculatePrice(cost: number, toppings: number) : number {
-  return (cost + 1.5 * toppings)
-  /// return (cost + 1.5 * toppings).toString();  
-    // will throw error because return is assigned number
-}
-const cost: number = calculatePrice(pizzaCost, pizzaToppings);
-console.log(`Pizza costs: ${cost}`);  /// use ` for ES string and ' for others
-
-//String type, String Literal
-
-const coupon: string = 'pizza25'; // double or single quote
-
-function normalizeCoupon(code: string): string{
-  return code.toUpperCase();
-}
-
-// const couponMessage: string =  `
-//   I am a string
-// `;
-// multi line string
-
-const couponMessage: string = `Final coupon is ${normalizeCoupon(coupon)}`;
-
-console.log(couponMessage);
-
-// Boolean Type Boolean Literal
-
-const item: number = 5;
-
-function offerDiscout(orders: number): boolean{
-  return orders >= 3;
-}
-
-if (offerDiscout(item)) {
-  console.log(`You're entitiled to a discount!`);
-} else {
-  console.log(`Order 3 or more pizzas for a discount`);
-}
+/*  */
