95bb394a7b69d663e297ee383adbe6197a6f2c10
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-12 11:25:01 -0700

    TypeScript Types section complete

--- src/app.ts
+++ src/app.ts
@@ -59,3 +59,73 @@ selectSize('medium');
 console.log(`Pizza size: ${pizzaSize}`);
 
 // works with numbers, bool, and enums as well
+
+/* Function Types */
+function sumOrder(price: number, quantity: number): number{
+  return price * quantity;
+}
+// let sumOrder2: Function;
+let sumOrder2: (price: number, quantity: number) => number;
+sumOrder2 = (x, y) => x * y;
+
+const sum = sumOrder(25, 2);
+const sum2 = sumOrder2(25, 2);
+
+console.log(`Total Sum 1: ${sum}`);
+console.log(`Total Sum 2: ${sum2}`);
+
+/* Functions and Optional Arguments */
+ // continuation of last section
+
+let sumOrder3: (price: number, quantity?: number) => number;
+sumOrder3 = (x, y) => {
+  if (y) {
+    return x * y;
+  }
+  return x;
+}
+
+const sum3 = sumOrder3(25);
+console.log(`Total Sum 3: ${sum3}`);
+
+/* Typed Functions and Default Parameters */
+// continuation of last section
+let sumOrder4: (price: number, quantity?: number) => number;
+// sumOrder4 = (x, y) => {
+//   const q = y || 1;
+//   return x * q;
+// }
+
+sumOrder4 = (x, y = 1) =>  x * y; 
+
+const sum4 = sumOrder4(25);
+console.log(`Total Sum 4: ${sum4}`);
+
+/* Object Types */
+
+let pizza: { name: string, price: number, getName(): string } = {
+  name: 'Plain old Pepperoni',
+  price: 20,
+  getName() {
+    return pizza.name;
+  }
+};
+
+console.log(pizza.getName());
+
+/* Array Types and Generics */
+
+let sizes: string[];
+sizes = ['small', 'medium', 'large'];
+
+let sizesNum: number[];
+sizesNum = [1, 2, 3];
+
+let toppingsArr: Array<string>; // requires type in < >
+toppingsArr = ['pepperoni', 'tomato', 'bacon'];
+
+/* Tuple Types for Arrays */
+const pizzaTupleEx = ['Pepperoni', 20, '', 20, true]; /// still valid but not what we want
+
+let pizzaTuple: [string, number, boolean];
+pizzaTuple = ['Pepperoni', 20, true]; // order matters
