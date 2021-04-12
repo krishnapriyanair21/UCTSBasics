10e463af539b3d037e376cbc9d3cb45b272a55af
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-12 10:46:49 -0700

    TypeScript Types Void

--- src/app.ts
+++ src/app.ts
@@ -5,4 +5,20 @@ coupon = 25; // number
 coupon = '25'; // string
 coupon = true;  // bool
 
-/*  */
+/* Implicit vs Explict Types */
+let implicitCoupon = 'pizza23';
+let explicitCoupon: string; 
+
+
+explicitCoupon = 'pizza25';
+
+/* Void Type */
+/// doesn't exist in Java Script
+let selectedTopping: string = 'pepperoni';
+
+function selectTopping(topping: string): void { 
+  selectedTopping = topping;
+} /// impure function
+
+selectTopping('bacon');
+console.log(selectedTopping);
