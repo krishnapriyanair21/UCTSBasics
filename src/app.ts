f7e2864f58c63396998e733a3347f228e38ec674
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-12 10:57:28 -0700

    TypeScript Types Null

--- src/app.ts
+++ src/app.ts
@@ -22,3 +22,32 @@ function selectTopping(topping: string): void {
 
 selectTopping('bacon');
 console.log(selectedTopping);
+
+/* Never Type */
+function orderError(error: string): never {
+  throw new Error(error);
+  // never going to return a value
+  // technically void 
+  // anything after line 28 will not execute
+}
+
+// orderError('Something went wrong');  // uncomment to check will not execute anything past line 35
+
+/* Null, Undefined, Strict Null checks */
+// change tsconfig.json strict to false
+
+// let coupon1:string = 'pizza20';
+let coupon1: string | null = 'pizza20'; 
+
+function removeCoupon(): void {
+  coupon1 = null;  // or undefined
+}
+
+console.log(coupon1);  // before null
+removeCoupon();
+console.log(coupon1); // after null 
+// change tsconfig.json strictNullChecks to true
+  // will not allow undefined for type string
+// change tsconfig.json strict to true
+
+ 
