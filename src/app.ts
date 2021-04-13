b5fb993b8feb90c4f9582e9f0b17b205da0ae7ab
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-13 09:46:51 -0700

    type assertions

--- src/app.ts
+++ src/app.ts
@@ -11,3 +11,13 @@ const selectSize: Callback = (x) => {
 selectSize('medium');
 
 /* Type Assertions */
+
+type Pizza = { name: string, toppings: number };
+const pizza: Pizza = { name: 'Blazing Inferno', toppings: 5 };
+const serialized = JSON.stringify(pizza);
+function getNameFromJSON(obj: string) {
+  // return (<Pizza>JSON.parse(obj)).name;  // old way of doing this
+  return (JSON.parse(obj) as Pizza).name; // newer way
+}
+
+getNameFromJSON(serialized);
