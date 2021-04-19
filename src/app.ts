62d53186ed94fd2296dbdeb34f82ce4d63e6c5d9
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-19 10:25:56 -0700

    interfaces with index signatures

--- src/app.ts
+++ src/app.ts
@@ -66,6 +66,31 @@
 
 /* Interfaces and Optional Properties */
 // continued
+// interface Sizes{
+//   sizes: string[];
+// }
+// interface Pizza extends Sizes { // inherit properties from Sizes
+//   name: string;
+//   toppings?: number;  // toppings is optional
+//   getAvailableSizes(): string[];
+// }
+
+// let pizza: Pizza;
+
+// function createPizza(name: string, sizes: string[]): Pizza {
+//   return {
+//     name,
+//     sizes,
+//     // toppings: 1,  // may not be what we want
+//     getAvailableSizes() { 
+//       return this.sizes;
+//     }
+//   };
+// }
+// pizza = createPizza('Pepperoni', ['small', 'medium']);
+// pizza.toppings = 1;
+
+/* Interfaces with Index Signatures */
 interface Sizes{
   sizes: string[];
 }
@@ -73,6 +98,9 @@ interface Pizza extends Sizes { // inherit properties from Sizes
   name: string;
   toppings?: number;  // toppings is optional
   getAvailableSizes(): string[];
+  [key: number]: string; // give data index signature
+  // number stored as string (i.e "1")
+  // [key: string]: any; 
 }
 
 let pizza: Pizza;
@@ -88,4 +116,5 @@ function createPizza(name: string, sizes: string[]): Pizza {
   };
 }
 pizza = createPizza('Pepperoni', ['small', 'medium']);
+pizza[1] = 'xyz'
 pizza.toppings = 1;
