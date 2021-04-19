1622d0ac20aad77d77b8ba56606e551fb3d174ca
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-19 10:16:07 -0700

    interfaces with function type

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 /* Creating Interfaces */
 
-interface Pizza {
-  name: string;
-  sizes: string[];
-}
+// interface Pizza {
+//   name: string;
+//   sizes: string[];
+// }
 
 // interface Pizzas{  // can use interface in making new interfaces
 //   data: Pizza[]
@@ -11,12 +11,31 @@ interface Pizza {
 
 let pizza: Pizza;
 
+// function createPizza(name: string, sizes: string[]): Pizza {
+//   return {
+//     name,
+//     sizes
+//     // deal: true // will cause error
+//   };
+// }
+
+pizza = createPizza('Pepperoni', ['small', 'medium']);
+
+/* Interfaces with Function Types */
+// continues from last code
+interface Pizza {
+  name: string;
+  sizes: string[];
+  getAvailableSizes(): string[];
+}
+// type getAvailableSizes = () => string[]; // observe difference bw type and interface
+
 function createPizza(name: string, sizes: string[]): Pizza {
   return {
     name,
-    sizes
-    // deal: true // will cause error
+    sizes,
+    getAvailableSizes() { // pass function with interface
+      return this.sizes;
+    }
   };
 }
-
-pizza = createPizza('Pepperoni', ['small', 'medium']);
